69425c4344ba903ff936281c71e3940759adfc6c
Aleksandr Kantserov <36185884+Nerzool1989@users.noreply.github.com>
2018-11-20 15:54:47 +0300

    Update hw2_normal.py

--- hw2_normal.py
+++ hw2_normal.py
@@ -33,3 +33,5 @@ date = '02.11.2013'
 date_total = datetime.date(datetime.strptime(date, '%d.%m.%Y'))
 date_total2 = date_total.strftime('%A %B %Y ' + "года")
 print(date_total2)
+
+Не успел больше...
